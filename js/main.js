aa3798229c8007c3d8604a6f112ad6a32c549f96
Leon Junio Martins Ferreira <juniohuy2gamer@gmail.com>
2025-07-20 01:20:39 -0300

    Blog v2 (remove vendors and one page scroll fork)

--- js/main.js
+++ js/main.js
@@ -9,18 +9,3 @@ window.addEventListener('DOMContentLoaded', function () {
         download();
     });
 });
-
-function check() {
-    let a = navigator.userAgent;
-    let b = document.getElementById("btn-down");
-    let d = document.getElementById("div-down");
-    text = `<h1>DOWNLOADS</h1>
-<hr>`;
-    if (a.includes("Edg") || a.includes("IE")) {
-        b.style.display = "none";
-        text += `<h1>UTILIZE O GOOGLE CHROME PARA REALIZAR O DOWNLOAD</h1>
-    <h4>Copie o link e cole no google chrome/firefox para iniciar o dowload</h4>
-    <p style="color:red;">O download via esse navegador esta bloqueado por questões de segurança e proteção à integridade do arquivo de download, por favor utilize o chrome ou o firefox para baixar e usar o sistema. Em caso de dúvidas contate o suporte do produto.</p>`;
-        d.innerHTML = text;
-    }
-}
